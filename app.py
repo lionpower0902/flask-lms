5783e1d7cdb6e8a1d4271518374144afd09a516c
lionpower0902 <teranaka@lion-power.net>
2025-03-28 12:35:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def watch(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
